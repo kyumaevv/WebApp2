52bd01b1d94e94d55a8628a6f39e0e05a1ac15c2
kyumaevv <91356416+kyumaevv@users.noreply.github.com>
2021-09-25 10:22:24 +0800

    Add files via upload

---
+++ lesson2/app.js
@@ -0,0 +1,23 @@
+$('#light').click(function(){
+	$('body').css('background-color',"powderblue");
+	$('.boxes').css('background-color',"cornsilk");
+	$('.content').css('background-color',"peachpuff");
+		$('.content').css('font-family',"serif");
+	$('body').css('color',"navy");
+})
+
+$('#dark').click(function(){
+	$('body').css('background-color',"black");
+	$('.boxes').css('background-color',"navy");
+	$('.content').css('background-color',"indigo");
+		$('.content').css('font-family',"serif");
+	$('body').css('color',"white");
+})
+
+$('#fancy').click(function(){
+	$('body').css('background-color',"lightslategray");
+	$('.boxes').css('background-color',"gray");
+	$('.content').css('background-color',"oldlace");
+	$('.content').css('font-family',"monospace");
+	$('body').css('color',"black");
+})
